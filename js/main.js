c88b84d726afcf1a457768f3d43311786b69ab15
vladick5513 <2006krickunow@gmail.com>
2024-04-05 00:51:26 +0300

    comment

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    let elements = document.querySelectorAll('.image, .text, .text_low');
+    elements.forEach(function(element) {
+        if (element.getBoundingClientRect().top < window.innerHeight) {
+            element.classList.add('show');
+        }
+    });
+});
